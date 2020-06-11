708408158dff56ddcc2c76af01cd971bcf67198a
Samplasion <dev@samplasion.js.org>
2020-06-11 10:23:58 +0200

    Add `console.log`s so that I can see the progress from GH Actions

--- twitter_status.js
+++ twitter_status.js
@@ -2,12 +2,16 @@ var Twitter = require('twitter');
 var fs = require("fs");
 const matter = require('gray-matter');
 
+console.log("Instantiating Twitter client...");
+
 const client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
+
+console.log("Instantiating new variables...");
 
 var date = new Date();
 var day = date.getDate()
@@ -18,9 +22,12 @@ var day = date.getDate()
 var dateString = `${year}-${pad(month)}-${pad(day)}`
   , post;
 
+console.log("Looking for file...");
+
 var file = fs.readdirSync("./_posts").filter(p => p.startsWith(dateString))[0];
 
 if (file) {
+    console.log("The file was found, extracting data from it...");
     var content = fs.readFileSync(`./_posts/${file}`);
 
     var slug = file.replace(dateString, "").substring(1).replace(".md", "");
@@ -31,14 +38,17 @@ if (file) {
     var excerpt = fm.data.excerpt || fm.content.split("<!--more-->")[0].trim();
 
     var tweet = `
-    New post on Samplasion's Blog: ${title}
-    https://blog.samplasion.js.org/${slug}
+New post on Samplasion's Blog: ${title}
+https://blog.samplasion.js.org/${slug}
     `;
+
+    console.log(`Tweeting:\n${tweet}`);
 
     client.post('statuses/update', {status: tweet},  function(error, tweet, response) {
         if(error) throw error;
-        console.log(tweet);  // Tweet body.
-        console.log(response);  // Raw response object.
+        console.log("Tweet sent!");
     });
 
+} else {
+  console.log("The file wasn't found; no post today.");
 }
