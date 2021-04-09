cc213503e76230772f07b2d108178d092cc5f66b
Samplasion <Samplasion@users.noreply.github.com>
2021-04-09 00:25:28 +0000

    09/04/21 00:25:28 - Scheduled Publish

---
+++ twitter_status.js
@@ -0,0 +1,54 @@
+var Twitter = require('twitter');
+var fs = require("fs");
+const matter = require('gray-matter');
+
+console.log("Instantiating Twitter client...");
+
+const client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+console.log("Instantiating new variables...");
+
+var date = new Date();
+var day = date.getDate()
+  , month = date.getMonth() + 1
+  , year = date.getFullYear()
+  , pad = (n) => n < 10 ? "0" + n : n;
+
+var dateString = `${year}-${pad(month)}-${pad(day)}`
+  , post;
+
+console.log("Looking for file...");
+
+var file = fs.readdirSync("./_posts").filter(p => p.startsWith(dateString))[0];
+
+if (file) {
+    console.log("The file was found, extracting data from it...");
+    var content = fs.readFileSync(`./_posts/${file}`);
+
+    var slug = file.replace(dateString, "").substring(1).replace(".md", "");
+
+    var fm = matter(content.toString());
+
+    var title = fm.data.title;
+    var excerpt = fm.data.excerpt || fm.content.split("<!--more-->")[0].trim();
+
+    var tweet = `
+Hey there! I've published a new post: "${title}". Check it out!
+https://blog.samplasion.js.org/${slug}
+    `;
+
+    console.log(`Tweeting:\n${tweet}\n`);
+
+    client.post('statuses/update', {status: tweet},  function(error, tweet, response) {
+        if(error) throw error;
+        console.log("Tweet sent!");
+    });
+
+} else {
+  console.log("The file wasn't found; no post today.");
+}
